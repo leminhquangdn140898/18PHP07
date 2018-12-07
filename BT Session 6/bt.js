5803dea2f698312df05db7394339d150969a15e6
leminhquangdn140898 <37803603+leminhquangdn140898@users.noreply.github.com>
2018-12-07 18:41:22 +0700

    ctnc

---
+++ BT Session 6/bt.js
@@ -0,0 +1,26 @@
+var a = 1;
+var b = 2;
+var t;
+function Cong( a , b) {
+	t = a + b;
+	document.write(' Tổng là :' + t );
+}
+Cong(1 , 2)
+
+function Tru( a , b) {
+	h = a - b;
+	document.write(' \n Hiệu là :' + h );
+}
+Tru(1 , 2)
+
+function Nhan( a , b) {
+	tich = a * b;
+	document.write(' \n Tích là :' + tich );
+}
+Nhan(1 , 2)
+
+function Chia (a , b) {
+	thuong = a / b;
+	document.write(' \n Thương là :' + thuong);
+}
+Chia (1 , 2)
--- BT Session 6/fibo.js
+++ BT Session 6/fibo.js
@@ -1,7 +1,7 @@
 var f1 = 0;
 var f2 = 1;
 var fn = f2;
-for (var i = 1; i <= 11 ; i++) {
+for (var i = 1; i <= 100 ; i++) {
 	document.write(fn + " ");
 	fn = f1 + f2;
 	f1 = f2;
